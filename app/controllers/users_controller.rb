e1d1d52f5a0d0c33da39c3a2b6ce1b90677b3f92
alexgont1 <alexgont1@gmail.com>
2019-03-10 20:41:12 -0500

    Add checks for errors when user edits profile

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,17 +1,16 @@
 class UsersController < ApplicationController
-  
+
   def show
     @user = User.find(params[:id])
   end
-  
+
   def new
     @user = User.new
   end
-  
+
   def create
     @user = User.new(user_params)
     if @user.save
-      # Handle a successful save.
       log_in @user
       flash[:success] = "Welcome to the Sample App!"
       redirect_to @user
@@ -19,12 +18,22 @@ class UsersController < ApplicationController
       render 'new'
     end
   end
-  
+
   def edit
     @user = User.find(params[:id])
   end
 
+  def update
+    @user = User.find(params[:id])
+    if @user.update_attributes(user_params)
+      # Handle a successful update.
+    else
+      render 'edit'
+    end
+  end
+
   private
+
     def user_params
       params.require(:user).permit(:name, :email, :password,
                                    :password_confirmation)
