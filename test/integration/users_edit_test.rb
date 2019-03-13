37728b4474764d6b447f7196fadcf2b0c13cacb6
alexgont1 <alexgont1@gmail.com>
2019-03-12 19:23:38 -0500

    Add files and tests to require login for unauthorized user

--- test/integration/users_edit_test.rb
+++ test/integration/users_edit_test.rb
@@ -7,6 +7,7 @@ class UsersEditTest < ActionDispatch::IntegrationTest
   end
 
   test "unsuccessful edit" do
+    log_in_as(@user)
     get edit_user_path(@user)
     assert_template 'users/edit'
     patch user_path(@user), params: { user: { name:  "",
@@ -15,5 +16,10 @@ class UsersEditTest < ActionDispatch::IntegrationTest
                                               password_confirmation: "bar" } }
 
     assert_template 'users/edit'
+  end
+  
+  test "successful edit" do
+    log_in_as(@user)
+    get edit_user_path(@user)
   end
 end
