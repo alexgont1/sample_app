7236694632fd38d72c42e55ca96a50191123f8ec
alexgont1 <alexgont1@gmail.com>
2019-03-29 12:49:52 -0500

    Add client side picture file size checking with jQuery

--- app/models/micropost.rb
+++ app/models/micropost.rb
@@ -11,7 +11,7 @@ class Micropost < ApplicationRecord
   # Validates the size of an uploaded picture.
   def picture_size
     if picture.size > 5.megabytes
-      errors.add(:picture, "should be less than 5MB")
+      errors.add(:picture, "should be less than 5MB") #add a custom error message to the errors collection
     end
   end
 end
